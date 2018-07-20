eb67f9d40edd51a8667df7abadd41f3773e013b2
Queco SM <sersalazar.marioni@gmail.com>
2018-07-20 00:47:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,15 +2,15 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 && int % 5 !== 0# if the number int is divisible by 3
      "Fizz" # Go fizz
 
 
-  elsif int % 5 == 0 # if the number int is divisible by 3
+  elsif int % 5 == 0 && int % 3 !== 0 # if the number int is divisible by 3
      "Buzz" # Go fizz
 
 
- elsif int % 15 == 0 # if the number int is divisible by 3
+ elsif int % 15 == 0  # if the number int is divisible by 3
     "FizzBuzz" # Go fizz
 
 
